86df6c3947b567e8a04f598afea17ea839a97d46
FlorinM11212 <Florin.HE23751@student.rcl.ac.uk>
2026-05-10 15:41:21 +0100

    Update test_fizzbuzz.py

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,31 +4,34 @@ from fizzbuzz import fizzbuzz
 
 
 class FizzBuzzTests(unittest.TestCase):
+    #First test.
     def test_returns_number_as_string_for_one(self):
         self.assertEqual(fizzbuzz(1), "1")
-
+    #Second test.
     def test_returns_number_as_string_for_two(self):
         self.assertEqual(fizzbuzz(2), "2")
-
+    #Third test.
     def test_returns_fizz_for_three(self):
         self.assertEqual(fizzbuzz(3), "Fizz")
-
+    #Fourth test.
     def test_returns_fizz_for_six(self):
         self.assertEqual(fizzbuzz(6), "Fizz")
-
+    #Fifth test.
     def test_returns_buzz_for_five(self):
         self.assertEqual(fizzbuzz(5), "Buzz")
-
+    #Sext test.
     def test_returns_buzz_for_ten(self):
         self.assertEqual(fizzbuzz(10), "Buzz")
-
+    #Seventh test.
     def test_returns_fizzbuzz_for_fifteen(self):
         self.assertEqual(fizzbuzz(15), "FizzBuzz")
-
+    # Eighth test.
     def test_returns_fizzbuzz_for_thirty(self):
         self.assertEqual(fizzbuzz(30), "FizzBuzz")
-
+    #Nineth test.
     def test_full_sequence_first_fifteen(self):
+
+        #Expected return. 
         expected = [
             "1", "2", "Fizz", "4", "Buzz",
             "Fizz", "7", "8", "Fizz", "Buzz",
