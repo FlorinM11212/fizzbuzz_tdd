1b23f3deb17845fc5459b4bb8f6b7a80f6236bcc
Florin <florin@example.com>
2026-05-10 14:38:55 +0100

    TEST: implement and pass divisible by 3 (Fizz) requirement

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -7,6 +7,15 @@ class FizzBuzzTests(unittest.TestCase):
     def test_returns_number_as_string_for_one(self):
         self.assertEqual(fizzbuzz(1), "1")
 
+    def test_returns_number_as_string_for_two(self):
+        self.assertEqual(fizzbuzz(2), "2")
+
+    def test_returns_fizz_for_three(self):
+        self.assertEqual(fizzbuzz(3), "Fizz")
+
+    def test_returns_fizz_for_six(self):
+        self.assertEqual(fizzbuzz(6), "Fizz")
+
 
 if __name__ == "__main__":
     unittest.main()
